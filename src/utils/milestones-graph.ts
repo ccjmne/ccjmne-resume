8a8d397fe6ccd410f911ea7c441af7b065f62bb5
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-09-07 01:20:10 +0200

    WIP: Polish milestones-graph.ts

--- src/utils/milestones-graph.ts
+++ src/utils/milestones-graph.ts
@@ -8,14 +8,14 @@ export function draw(milestones: string[], range: Range): EasyHTMLElement[] {
 }
 
 type Branch = {
-  depth: number,
+  depth:  number,
   events: Array<{ pos: number, type: string, label: string }>,
 }
 
 // TODO: Do I need the years?
 // TODO: Also, wouldn't I rather have the milestones in reverse chronological order in profile.json?
 const MILESTONE_PARSER = /^(?<year>\d{4}) (?<pipes>[☆★│├┐┘╵]+)\s*(?<label>.*?)\s*$/ as MatcherWGroups<'year' | 'pipes' | 'label'>
-const [NEW, MILESTONE, HIGHLIGHT, MERGE, END] = ['┐', '☆', '★', '┘', '╵']
+const [HIGHLIGHT, MILESTONE, NEW, MERGE, END] = ['★', '☆', '┐', '┘', '╵']
 
 function compute(milestones: string[]): Branch[] {
   const branches: Branch[] = [{ depth: 0, events: [] }]
@@ -40,35 +40,32 @@ function compute(milestones: string[]): Branch[] {
 
 function contextualise(domain: Range, range: Range): (branch: Branch) => EasyHTMLElement {
   const unitX = 20 // TODO: get from scss
-  const scale = piecewise(domain, range)
 
-  return function draw({ depth: col, events }: Branch): EasyHTMLElement {
+  const map      = zip(domain, range)
+  const segments = zip(map.slice(0, -1), map.slice(1))
+
+  function scale(at: number): number { // Piecewise linear scale
+    const [[x0, y0], [x1, y1]] = segments.findLast(([[a]]) => at >= a)!
+    return (at - x0) * ((y1 - y0) / (x1 - x0)) + y0
+  }
+
+  return function draw({ depth, events }: Branch): EasyHTMLElement {
     const [first, last] = [events.at(0)!, events.at(-1)!]
     const colour = Math.floor(Math.random() * (1 << 6) + (1 << 7)) // TODO: make deterministic (also do in scss)
 
-    return elementSVG('g').attrs({ transform: `translate(${-col * unitX})` }).content(
+    return elementSVG('g').attrs({ transform: `translate(${-depth * unitX})` }).content(
       elementSVG('path').attrs({
         fill: 'none', stroke: `rgb(${colour}, ${colour}, ${colour})`, 'stroke-width': '5px', d: `M0,${scale(first.pos)}`
           + (first.type === NEW ? `m${unitX},0 h${-(unitX - 10)} a10,10 0 0,1 ${-10}, -10` : 'v10')
           + `V${scale(last.pos) + 10}` + (last.type === MERGE ? `a10,10 0 0,1 ${10},-10 h${(unitX - 10)} ` : 'v-10') // TODO: fizzle out at end
       }),
-      ...events.filter(({ type }) => [MILESTONE, HIGHLIGHT].includes(type)).map(({ pos: at, type }) => elementSVG('circle')
-        .attrs({ r: 7, cx: 0, cy: scale(at), fill: 'white', 'stroke-width': '5', stroke: type === HIGHLIGHT ? 'hsl(185 52% 33% / 1)' : `rgb(${colour}, ${colour}, ${colour})` })),
-      ...events.map(({ pos: at, label: label }) => elementSVG('text')
+      ...events.filter(({ type }) => [MILESTONE, HIGHLIGHT].includes(type)).map(({ pos, type }) => elementSVG('circle')
+        .attrs({ r: 7, cx: 0, cy: scale(pos), fill: 'white', 'stroke-width': '5', stroke: type === HIGHLIGHT ? 'hsl(185 52% 33% / 1)' : `rgb(${colour}, ${colour}, ${colour})` })),
+      ...events.map(({ pos, label: label }) => elementSVG('text')
         .styles({ 'font-size': 'smaller' })
-        .attrs({ x: -unitX / 2, y: scale(at), 'text-anchor': 'end', 'dominant-baseline': 'middle' })
+        .attrs({ x: -unitX / 2, y: scale(pos), 'text-anchor': 'end', 'dominant-baseline': 'middle' })
         .content(label)),
     )
-  }
-}
-
-function piecewise(domain: Range, range: Range): (at: number) => number {
-  const map = zip(domain, range)
-  const segments = zip(map.slice(0, -1), map.slice(1))
-
-  return function scale(at: number): number {
-    const [[x0, y0], [x1, y1]] = segments.findLast(([[a]]) => at >= a)!
-    return (at - x0) * ((y1 - y0) / (x1 - x0)) + y0
   }
 }
 
