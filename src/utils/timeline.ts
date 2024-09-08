fa78d6801a9c9ab1202e4a319858f8b8f9da12e1
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-09-08 19:21:56 +0200

    WIP: Use SVG mask to polish timeline graph
    There's a problem, however: when rendered at 100% (without zooming in),
    it appears that the resulting PDF is not quite right in Chromium.  It is
    rendered well in Firefox.  Zooming in in Chromium does remedy this
    issue, which means that the problem really is with Chromium's rendering
    rather than with the PDF itself at the time of generation.
    
    I tried a couple of things:
    
    - rendering it with all versions of Puppeteer until a version so ancient
      it wouldn't even render SVGs at all (<19.0.0),
    - rendering it using free online HTML-to-PDF converters,
    - placing the `mask` in its own `defs` at the top-level of the current
      svg...
    
    To no avail.  I haven't thought of a valid solution yet.  Well, I could
    consider creating a different mask for each "node", which may possibly
    work... But it's pretty lame, especially considering that the maths is
    right and all aligns precisely right in principle and that the rendering
    issue only resides at the *consumer level*.
    
    Also make its branches thinner: 4px instead of 5.  Using an even number
    has the advantage of also presenting a crisper edge when rendering, it
    seems.
    
    Additionally round the stroke-linecap.

--- src/utils/timeline.ts
+++ src/utils/timeline.ts
@@ -31,35 +31,53 @@ export function render(timeline: string[], range: number[]): EasyHTMLElement[] {
   const domain   = [timeline.length, ...timeline.map((c, i) => [c, timeline.length - i - 1] as const).filter(([c]) => c.includes(HIGHLIGHT)).map(([, i]) => i), 0]
   const map      = zip(domain, range).toReversed()
   const segments = zip(map.slice(0, -1), map.slice(1))
+  const branches = compute(timeline.toReversed())
 
   function scale(at: number): number { // Piecewise linear scale
     const [[x0, y0], [x1, y1]] = segments.findLast(([[a]]) => at >= a)!
     return (at - x0) * ((y1 - y0) / (x1 - x0)) + y0
   }
 
-  return compute(timeline.toReversed()).map(function({ depth, events }: Branch): EasyHTMLElement {
-    const [first, last] = [events.at(0)!, events.at(-1)!]
-    const colour = Math.floor(Math.random() * (1 << 6) + (1 << 7)) // TODO: make deterministic (also do in scss)
+  const colours = branches.map(() => Math.floor(Math.random() * (1 << 6) + (1 << 7))) // TODO: make deterministic (also do in scss)
+  const mask = elementSVG('mask').attrs({ id: 'timeline-mask' }).content(
+    elementSVG('rect').attrs({ x: -10000, y: 0, width: 20000, height: 10000, fill: '#fff' }),
+    ...branches.flatMap(({ events, depth }) => events.map(e => ({ depth, ...e }))
+      .filter(({ type }) => [MILESTONE, HIGHLIGHT].includes(type))
+      .flatMap(({ pos, type }) => [
+        elementSVG('circle').attrs({ r: 2.5, cx: -depth * UNIT_X, cy: scale(pos), fill: '#000' }),
+        ...type === HIGHLIGHT ? [] : [elementSVG('path').attrs({ d: `M${-depth * UNIT_X - 10},${scale(pos)} h-200`, stroke: '#000', 'stroke-width': '10px' })],
+      ])
+    ))
 
-    return elementSVG('g').attrs({ transform: `translate(${-depth * UNIT_X})` }).content(
+  const graph = elementSVG('g').attrs({ mask: `url(#timeline-mask)` }).content(...branches.flatMap(function({ depth, events }: Branch, i): EasyHTMLElement[] {
+    const [first, last] = [events.at(0)!, events.at(-1)!]
+    const colour = colours[i]
+    return [elementSVG('path').attrs({
+      'stroke-linecap': 'round', fill: 'none', transform2: `translate(${-depth * UNIT_X})`, stroke: `rgb(${colour}, ${colour}, ${colour})`, 'stroke-width': '4px',
+      d: `M${-depth * UNIT_X},${scale(first.pos)}`
+        + (first.type === NEW ? `m${UNIT_X},0 h${-(UNIT_X - 10)} a10,10 0 0,1 ${-10},-10` : 'v10')
+        + `V${scale(last.pos) + 10}` + (last.type === MERGE ? `a10,10 0 0,1 ${10},-10 h${(UNIT_X - 10)} ` : 'v-10') // TODO: fizzle out at end
+    }),
+    ...events.filter(({ type }) => [MILESTONE, HIGHLIGHT].includes(type)).map(({ pos, type }) => elementSVG('circle')
+      .attrs({ r: 7, cx: -depth * UNIT_X, cy: scale(pos), fill: type === HIGHLIGHT ? 'hsl(185 52% 33% / 1)' : `rgb(${colour}, ${colour}, ${colour})` })),
+    ]
+  }))
+
+  const labels = branches.flatMap(function({ depth, events }: Branch, i): EasyHTMLElement[] {
+    const colour = colours[i]
+    return events.filter(({ label }) => !!label).flatMap(({ pos, label, xsection }) => [
       elementSVG('path').attrs({
-        fill: 'none', stroke: `rgb(${colour}, ${colour}, ${colour})`, 'stroke-width': '5px', d: `M0,${scale(first.pos)}`
-          + (first.type === NEW ? `m${UNIT_X},0 h${-(UNIT_X - 10)} a10,10 0 0,1 ${-10}, -10` : 'v10')
-          + `V${scale(last.pos) + 10}` + (last.type === MERGE ? `a10,10 0 0,1 ${10},-10 h${(UNIT_X - 10)} ` : 'v-10') // TODO: fizzle out at end
+        d: `M${-depth * UNIT_X - UNIT_X / 2},${scale(pos)} h${(depth - xsection) * UNIT_X + 1.5 * UNIT_X / 2}`,
+        stroke: `rgb(${colour}, ${colour}, ${colour})`, 'stroke-dasharray': '5 0', 'stroke-width': '2px',
       }),
-      ...events.filter(({ type }) => [MILESTONE, HIGHLIGHT].includes(type)).map(({ pos, type }) => elementSVG('circle')
-        .attrs({ r: 7, cx: 0, cy: scale(pos), fill: 'white', 'stroke-width': '5', stroke: type === HIGHLIGHT ? 'hsl(185 52% 33% / 1)' : `rgb(${colour}, ${colour}, ${colour})` })),
-      ...events.filter(({ label }) => !!label).flatMap(({ pos, label, xsection }) => [
-        elementSVG('line').attrs({
-          stroke: `red`, 'stroke-dasharray': '5 3', 'stroke-width': '1px', x2: 0 - UNIT_X / 2, x1: UNIT_X / 8 + UNIT_X * (depth - xsection), y2: scale(pos), y1: scale(pos)
-        }),
-        elementSVG('text')
-          .styles({ 'font-size': 'smaller' })
-          .attrs({ x: UNIT_X * (depth - xsection), y: scale(pos), 'text-anchor': 'end', 'dominant-baseline': 'middle' })
-          .content(label)
-      ]),
-    )
+      elementSVG('text')
+        .styles({ 'font-size': 'smaller' })
+        .attrs({ x: UNIT_X * -xsection, y: scale(pos), 'text-anchor': 'end', 'dominant-baseline': 'middle' })
+        .content(label)
+    ])
   })
+
+  return [mask, graph, ...labels]
 }
 
 function zip<A, B>(a: A[], b: B[]): Array<[A, B]> {
