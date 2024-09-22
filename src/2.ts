c4a40768c3cd9533d804a46d84915e0fe056b836
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-09-22 22:04:18 +0200

    Add right-aligned SVG background to 2nd page title

--- src/2.ts
+++ src/2.ts
@@ -1,22 +1,24 @@
-import { article, div, element, lightest, section, span } from "utils/easy-htmlelement"
+import { article, div, element, elementSVG, lightest, section, span } from "utils/easy-htmlelement"
 
 import './scss/2/2.scss'
 import profile from './profile.json'
 import qrcode from 'qrcode'
 import { MatchArrayWGroups } from "types"
 import { render } from "utils/timeline"
-import { rhombus } from "utils/svg-elements"
+import { rhombus, titlebar } from "utils/svg-elements"
 
 const { highlights, timeline } = profile
 
 const graph = element()
 const qcode = element('img')
+const mask = elementSVG('mask').attrs({ id: 'main-background-mask' })
 
 element(document.body).content(
   element('main').content(
     graph.at('graph'),
     element().at('highlights').content(
-      element('h2').content('Career Highlights').at('title').styles({ 'text-align': 'right', margin: 0 }),
+      elementSVG().attrs({ width: 0, height: 0 }).content(elementSVG('defs').content(mask)),
+      element('h2').content('Career Highlights').at('title'),
       section('highlights').content(
         ...highlights.map(({ content, dates, numbers, headline }) => article('highlight').content(
           element('h3').content(headline).at('headline'),
@@ -42,13 +44,23 @@ element(document.body).content(
 
 // TODO: prefix with https://, move/rename project to github.com/ccjmne/resume (drop ccjmne- prefix in project name)
 const data = await qrcode.toString('ccjmne.github.io/ccjmne-resume', { type: 'svg', errorCorrectionLevel: 'L', version: 2, margin: 0 })
-qcode.attrs({ height: 'auto', src: `data:image/svg+xml;utf8,${(data.replace(/#f+/, 'transparent').replace(/#0+/, encodeURIComponent('#eee')))}` })
+qcode.attrs({ height: 'auto', src: `data:image/svg+xml;utf8,${data.replace(/#f+/, 'transparent').replace(/#0+/, encodeURIComponent('#eee'))}` })
 
 document.fonts.ready.then(async function () {
-  const { top, height } = document.querySelector('[grid-area=highlights]')!.getBoundingClientRect()
+  const { top, height }                = document.querySelector('[grid-area=highlights]')!.getBoundingClientRect()
+  const { top: ftop }                  = document.querySelector('footer')!.getBoundingClientRect()
+  const { top: ttop, height: theight } = document.querySelector('main h2')!.getBoundingClientRect()
   const highlights = [...document.querySelectorAll('[grid-area=headline]')]
     .map(e => e.getBoundingClientRect())
     .map(({ top: t, height: h }) => t - top + h / 2)
 
   graph.content(render(timeline, highlights, height))
+  mask.content(
+    elementSVG('rect').attrs({ x: 0, y: 0,    width: 9999, height: 9999, fill: '#fff' }),
+    elementSVG('rect').attrs({ x: 0, y: ftop, width: 9999, height: 9999, fill: '#000' }),
+    // This actually would "hollow out" the SVG instead of printing it in (off-)white.
+    //element(((new DOMParser().parseFromString(data.replace(/#f+/, 'transparent').replace(/#0+/, encodeURIComponent('#000')), 'image/svg+xml')).querySelector('path:last-child') as SVGPathElement))
+    //  .attrs({ stroke: '#fff', transform: 'translate(669 998.171875) scale(4)' }),
+    titlebar({ seed: 42, x: 500, y: ttop, h: theight, align: 'right', separator: false }),
+  )
 })
