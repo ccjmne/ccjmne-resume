bf60097355ccb790dacec2a39f14ab04d9d01e77
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-10-13 20:50:05 +0200

    Tweak seed for background of first h2 title

--- src/1.ts
+++ src/1.ts
@@ -80,7 +80,7 @@ element(document.body).content(
 document.fonts.ready.then(() => mask.content(
   elementSVG('rect').attrs({ x: 0, y: 0, width: 9999, height: 9999, fill: '#fff' }),
   ...([...document.querySelectorAll('main h2')] as HTMLElement[]).map(({ offsetHeight, offsetTop }, i) => titlebar({
-    seed: i ? 42 : 2017-3-10,
+    seed: i ? 42 : 0x1990_05_15 - 0x2017_3_10,
     x:    260,
     y:    offsetTop,
     h:    offsetHeight
