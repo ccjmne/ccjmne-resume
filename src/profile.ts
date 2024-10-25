ec0baddd076c128477dd45f03ef3c6d84c263850
Éric NICOLAS <ccjmne@gmail.com>
2024-10-26 00:41:14 +0200

    Order profile.ts sections semantically

--- src/profile.ts
+++ src/profile.ts
@@ -1,83 +1,8 @@
-export const timeline = `
-╷╷╷
-☆││ Start using [Neovim](https://neovim.io/)
-││★
-☆││ Start using [Rust](https://www.rust-lang.org/)<br>for personal projects
-☆││ Switch to [Arch Linux](https://archlinux.org/)
-│☆│ Migrate Cloud resources<br>to [ARM architecture](https://en.wikipedia.org/wiki/ARM_architecture_family)
-││★
-☆││ Mastery of Git
-│☆│ Excellent [AWS](https://aws.amazon.com/) proficiency
-││☆ Join [Unite](https://unite.eu/en-global)
-├┼┘
-☆│  Mastery of [RxJs](https://rxjs.dev/)
-│★
-☆│  Mastery of TypeScript
-│☆  Mastery of [D3.js](https://d3js.org/)
-│☆  Migrate services to [IPv6](https://en.wikipedia.org/wiki/IPv4_address_exhaustion)
-☆│  Start contributing to [the TC39](https://tc39.es/)
-│★
-│☆  Mastery of AngularJS
-│☆  Begin freelancing
-├┘╷
-├┐☆ Graduation of [my apprentice](https://uk.linkedin.com/in/maxime-endewelt-320ab79a)
-│☆│ Excellent JavaScript proficiency
-│★│
-☆││ Switch to [Ubuntu](https://ubuntu.com/)
-│☆│ Join [Propellerhead](https://www.propellerhead.co.nz/)
-├┘│
-├┐│
-│☆│ Excellent Java proficiency
-│☆│ First lead developer role
-│★│
-││☆ First mentor role
-│├┘
-│☆  Skilled C++ proficiency
-│☆  Join [Safran Aircraft Engines](https://www.safran-group.com/companies/safran-aircraft-engines)
-├┘
-☆   Start using [Debian](https://www.debian.org/)
-╵
-`.trim().split(/\n/g)
-
-export const techstack = {
-  Web:      ['Webpack', 'RxJs', 'NodeJs', 'Angular', 'Vite', 'Lit', 'Tailwind', 'GraphQL', 'React'],
-  Database: ['PostgreSQL', 'jOOQ', 'DynamoDB', 'Liquibase', 'RDS', 'OracleDB', 'SQLite'],
-  Code:     ['Neovim', 'TypeScript', 'Java', 'Visual Studio Code', 'JavaScript', 'Git', 'Lua', 'C++'],
-  Tools:    ['tmux', 'Git', 'Linux', 'ripgrep', 'fd', 'Zsh', 'AWK', 'jq', 'fzf', 'asdf', 'powerlevel10k'],
-  Cloud:    ['AWS', 'Google Cloud Platform', 'GitHub Actions', 'Kubernetes', 'Terraform'],
+export const identity = {
+  name:    'Éric NICOLAS',
+  title:   'Solutions Architect',
+  aboutme: "I am a strategic and detail-oriented problem solver with an acutely critical mind, driven by a passion for innovation. In my experience, most of a project's value is derived from adequately identifying and contextualising the problem to answer—this is where I have the greatest impact.\nIn designing systems, I believe that [clarity](https://en.wikipedia.org/wiki/Worse_is_better) and [elegance](https://en.wikipedia.org/wiki/Unix_philosophy) are key to a lasting significance: I am a stalwart advocate of radical simplicity, and strive for creative solutions that will remain effective and adaptable.",
 }
-
-export const highlights = [{
-  dates:    '2024',
-  headline: 'Steered project back on track to meet crucial deadline',
-  content:  'I led the initiative to revise the design of a project gone off-course and coordinated the efforts to deliver on time a future-proof solution that would address the imminent amendments to the [Visa](https://usa.visa.com/) compliance mandate.',
-  numbers:  ["corrected course of project\nof *6 months'* lead time", "with under *3 weeks*\nbefore pivotal cutoff date"],
-}, {
-  dates:    '2023',
-  headline: 'Spearheaded redesign of dysfunctional payment workflow',
-  content:  'I drove the complete overhaul of the procedure and architecture of the credit card payment system at a [major B2B e-procurement platform](https://unite.eu/), and provided a working implementation that purged accidental complexity and addressed discovered vulnerabilities.',
-  numbers:  ['reduced code base\nby about *96%*', 'for platform handling annually\n*billions of euros* in transactions'],
-}, {
-  dates:    '2020',
-  headline: 'Resolved performance bottleneck in data computation engine',
-  content:  "I proposed to [better leverage a customer's database engine](https://blog.jooq.org/truth-first-or-why-you-should-mostly-implement-database-first-designs/) and pair intricate [SQL queries](https://en.wikipedia.org/wiki/SQL) with savvy and straightforward API design to break the impasse of a solution having far outgrown its original scope.",
-  numbers:  ['from about 15 seconds\ndown to *80 milliseconds*', 'entirely eliminated\nthe *reliance on caching*'],
-}, {
-  dates:    '2016',
-  headline: 'Awarded public tender through innovation partnership',
-  content:  'I designed a bespoke [EHS solution](https://en.wikipedia.org/wiki/Environment,_health_and_safety) lauded by a [digital incubator of the French government](https://beta.gouv.fr/) and was funded for its implementation and continuous development.',
-  numbers:  ['used by over *9,000*\ngovernment employees', 'ongoing partnership\nof nearly *9 years*'],
-}, {
-  dates:    '2014',
-  headline: 'Built data contextualisation protocol for the [Semantic Web](https://en.wikipedia.org/wiki/Semantic_Web)',
-  content:  "I jointly designed as well as implemented the procedure and supporting system by which [Auckland Museum](https://www.aucklandmuseum.com/)'s collections curator could map out the semantic attributes of their [Linked Open Data](https://en.wikipedia.org/wiki/Linked_data) platform's ever-growing digital archive.",
-  numbers:  ['implemented for the *Web 3.0*\nover a decade ago', 'highlighted\nby *the W3C*'],
-}, {
-  dates:    '2012',
-  headline: 'Identified and addressed costly, recurring resource waste',
-  content:  'I noticed an opportunity for, designed, built and drove the promotion of a workflow optimisation for technicians at a [world-leading aircraft engines manufacturer](https://www.safran-group.com/companies/safran-aircraft-engines) to substantially reduce the strenuous cost of late-stage testing of our propulsion systems.',
-  numbers:  ['over *1,000,000 euros*\nsaved each year', 'improved longstanding procedure\nin the *defence industry*'],
-}]
 
 export const links = [{
   icon: 'mail.svg',
@@ -96,12 +21,6 @@ export const links = [{
   href: 'https://github.com/ccjmne',
   text: 'github/ccjmne',
 }]
-
-export const identity = {
-  name:    'Éric NICOLAS',
-  title:   'Solutions Architect',
-  aboutme: "I am a strategic and detail-oriented problem solver with an acutely critical mind, driven by a passion for innovation. In my experience, most of a project's value is derived from adequately identifying and contextualising the problem to answer—this is where I have the greatest impact.\nIn designing systems, I believe that [clarity](https://en.wikipedia.org/wiki/Worse_is_better) and [elegance](https://en.wikipedia.org/wiki/Unix_philosophy) are key to a lasting significance: I am a stalwart advocate of radical simplicity, and strive for creative solutions that will remain effective and adaptable.",
-}
 
 export const skills = [
   ['AWS Platform', 'Arch. Design'],
@@ -179,3 +98,84 @@ export const endorsments = [{
   title:   'University Professor',
   excerpt: 'Éric is a hard-working person, tackling all assignments with dedication.\nI highly recommend him, he is a team player and would make a great asset to any organization.',
 }]
+
+export const highlights = [{
+  dates:    '2024',
+  headline: 'Steered project back on track to meet crucial deadline',
+  content:  'I led the initiative to revise the design of a project gone off-course and coordinated the efforts to deliver on time a future-proof solution that would address the imminent amendments to the [Visa](https://usa.visa.com/) compliance mandate.',
+  numbers:  ["corrected course of project\nof *6 months'* lead time", "with under *3 weeks*\nbefore pivotal cutoff date"],
+}, {
+  dates:    '2023',
+  headline: 'Spearheaded redesign of dysfunctional payment workflow',
+  content:  'I drove the complete overhaul of the procedure and architecture of the credit card payment system at a [major B2B e-procurement platform](https://unite.eu/), and provided a working implementation that purged accidental complexity and addressed discovered vulnerabilities.',
+  numbers:  ['reduced code base\nby about *96%*', 'for platform handling annually\n*billions of euros* in transactions'],
+}, {
+  dates:    '2020',
+  headline: 'Resolved performance bottleneck in data computation engine',
+  content:  "I proposed to [better leverage a customer's database engine](https://blog.jooq.org/truth-first-or-why-you-should-mostly-implement-database-first-designs/) and pair intricate [SQL queries](https://en.wikipedia.org/wiki/SQL) with savvy and straightforward API design to break the impasse of a solution having far outgrown its original scope.",
+  numbers:  ['from about 15 seconds\ndown to *80 milliseconds*', 'entirely eliminated\nthe *reliance on caching*'],
+}, {
+  dates:    '2016',
+  headline: 'Awarded public tender through innovation partnership',
+  content:  'I designed a bespoke [EHS solution](https://en.wikipedia.org/wiki/Environment,_health_and_safety) lauded by a [digital incubator of the French government](https://beta.gouv.fr/) and was funded for its implementation and continuous development.',
+  numbers:  ['used by over *9,000*\ngovernment employees', 'ongoing partnership\nof nearly *9 years*'],
+}, {
+  dates:    '2014',
+  headline: 'Built data contextualisation protocol for the [Semantic Web](https://en.wikipedia.org/wiki/Semantic_Web)',
+  content:  "I jointly designed as well as implemented the procedure and supporting system by which [Auckland Museum](https://www.aucklandmuseum.com/)'s collections curator could map out the semantic attributes of their [Linked Open Data](https://en.wikipedia.org/wiki/Linked_data) platform's ever-growing digital archive.",
+  numbers:  ['implemented for the *Web 3.0*\nover a decade ago', 'highlighted\nby *the W3C*'],
+}, {
+  dates:    '2012',
+  headline: 'Identified and addressed costly, recurring resource waste',
+  content:  'I noticed an opportunity for, designed, built and drove the promotion of a workflow optimisation for technicians at a [world-leading aircraft engines manufacturer](https://www.safran-group.com/companies/safran-aircraft-engines) to substantially reduce the strenuous cost of late-stage testing of our propulsion systems.',
+  numbers:  ['over *1,000,000 euros*\nsaved each year', 'improved longstanding procedure\nin the *defence industry*'],
+}]
+
+export const timeline = `
+╷╷╷
+☆││ Start using [Neovim](https://neovim.io/)
+││★
+☆││ Start using [Rust](https://www.rust-lang.org/)<br>for personal projects
+☆││ Switch to [Arch Linux](https://archlinux.org/)
+│☆│ Migrate Cloud resources<br>to [ARM architecture](https://en.wikipedia.org/wiki/ARM_architecture_family)
+││★
+☆││ Mastery of Git
+│☆│ Excellent [AWS](https://aws.amazon.com/) proficiency
+││☆ Join [Unite](https://unite.eu/en-global)
+├┼┘
+☆│  Mastery of [RxJs](https://rxjs.dev/)
+│★
+☆│  Mastery of TypeScript
+│☆  Mastery of [D3.js](https://d3js.org/)
+│☆  Migrate services to [IPv6](https://en.wikipedia.org/wiki/IPv4_address_exhaustion)
+☆│  Start contributing to [the TC39](https://tc39.es/)
+│★
+│☆  Mastery of AngularJS
+│☆  Begin freelancing
+├┘╷
+├┐☆ Graduation of [my apprentice](https://uk.linkedin.com/in/maxime-endewelt-320ab79a)
+│☆│ Excellent JavaScript proficiency
+│★│
+☆││ Switch to [Ubuntu](https://ubuntu.com/)
+│☆│ Join [Propellerhead](https://www.propellerhead.co.nz/)
+├┘│
+├┐│
+│☆│ Excellent Java proficiency
+│☆│ First lead developer role
+│★│
+││☆ First mentor role
+│├┘
+│☆  Skilled C++ proficiency
+│☆  Join [Safran Aircraft Engines](https://www.safran-group.com/companies/safran-aircraft-engines)
+├┘
+☆   Start using [Debian](https://www.debian.org/)
+╵
+`.trim().split(/\n/g)
+
+export const techstack = {
+  Web:      ['Webpack', 'RxJs', 'NodeJs', 'Angular', 'Vite', 'Lit', 'Tailwind', 'GraphQL', 'React'],
+  Database: ['PostgreSQL', 'jOOQ', 'DynamoDB', 'Liquibase', 'RDS', 'OracleDB', 'SQLite'],
+  Code:     ['Neovim', 'TypeScript', 'Java', 'Visual Studio Code', 'JavaScript', 'Git', 'Lua', 'C++'],
+  Tools:    ['tmux', 'Git', 'Linux', 'ripgrep', 'fd', 'Zsh', 'AWK', 'jq', 'fzf', 'asdf', 'powerlevel10k'],
+  Cloud:    ['AWS', 'Google Cloud Platform', 'GitHub Actions', 'Kubernetes', 'Terraform'],
+}
