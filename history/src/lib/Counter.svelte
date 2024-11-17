beed019400f137ea34148843b5f9e2c6c53f3915
Éric NICOLAS <ccjmne@gmail.com>
2024-11-17 18:00:05 +0100

    Prepare Svelte app for history
    Using Vite without SvelteKit, as well as SCSS and TypeScript.

---
+++ history/src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count = $state(0)
+  function increment(): void {
+    count += 2;
+  }
+</script>
+
+<button onclick={increment}>
+  count is {count}
+</button>
---
+++ history/svelte.config.js
@@ -0,0 +1,9 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static'
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  }
+}
