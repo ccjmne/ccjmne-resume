30099591393a0708d46cec59c2b25d8bf1a48591
Éric NICOLAS <ccjmne@gmail.com>
2024-11-17 18:08:07 +0100

    Prune starter app

--- history/src/App.svelte
+++ history/src/App.svelte
@@ -1,19 +1,9 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>ccjmne-resume</h1>
 
   <div class="card">
     <Counter />
@@ -29,20 +19,6 @@
 </main>
 
 <style lang="scss">
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-
-    &:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    &:hover {
-      filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-  }
   .read-the-docs {
     color: #888;
   }
--- history/src/main.js
+++ history/src/main.js
@@ -1,5 +1,5 @@
 import { mount } from 'svelte'
-import './app.css'
+import './app.scss'
 import App from './App.svelte'
 
 const app = mount(App, {
