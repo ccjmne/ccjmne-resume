691f25e3b6f0328bd3d439e8f3d166a4c26aeb94
Éric NICOLAS <ccjmne@gmail.com>
2024-11-17 18:16:12 +0100

    Switch from main.js to main.ts

--- history/src/main.ts
+++ history/src/main.ts
@@ -3,7 +3,7 @@ import './app.scss'
 import App from './App.svelte'
 
 const app = mount(App, {
-  target: document.getElementById('app'),
+  target: document.getElementById('app')!,
 })
 
 export default app
