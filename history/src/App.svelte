6e8346d82b7a75abceb555769ac71e098596ece1
Éric NICOLAS <ccjmne@gmail.com>
2024-11-22 21:54:06 +0100

    Preprocess PDF screenshots into thumbnails

--- history/src/App.svelte
+++ history/src/App.svelte
@@ -1,12 +1,19 @@
 <script>
   import Resume from './lib/Resume.svelte'
 
-  const resumes = Object.keys(
-    import.meta.glob('/public/assets/*/*.pdf', { eager: true })
-  ).map(pdf => ({
-    version: pdf.split('/').slice(-2, -1)[0],
-    pdf,
-    thumbnail: pdf.replace(/.pdf$/, '.png'),
+  const thumbnails = import.meta.glob('/src/assets/*.png', {
+    eager: true,
+    import: 'default',
+    query: { url: true, h: 250 },
+  })
+
+  const resumes = Object.values(
+    import.meta.glob('/src/assets/*.ts', { eager: true })
+  ).map(({ version, date, resume, thumbnail }) => ({
+    version,
+    date,
+    pdf: import.meta.resolve(`/assets/${version}.pdf`),
+    thumbnail: thumbnails[`/src/assets/${version}.png`],
   }))
 </script>
 
---
+++ history/src/assets/1.0.1.ts
@@ -0,0 +1,2 @@
+export const version = '1.0.1'
+export const date = new Date('2022-06-01')
