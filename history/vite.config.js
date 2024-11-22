d42cdfb0b56e00b59a455a7a2d80b3a3743c8bec
Éric NICOLAS <ccjmne@gmail.com>
2024-11-22 22:35:02 +0100

    Set up glsl files pipeline

--- history/vite.config.js
+++ history/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { imagetools } from 'vite-imagetools'
+import { default as glsl } from 'vite-plugin-glsl'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte(), imagetools()],
+  plugins: [svelte(), imagetools(), glsl()],
   css: {
     preprocessorOptions: { scss: { api: 'modern' } }
   },
