b4625d5e827fcf7e29184802d33067cf8c35c33e
Éric NICOLAS <ccjmne@gmail.com>
2024-11-24 20:13:53 +0100

    Minify GLSL for production builds

--- history/vite.config.ts
+++ history/vite.config.ts
@@ -3,9 +3,9 @@ import { imagetools } from 'vite-imagetools'
 import { default as glsl } from 'vite-plugin-glsl'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-export default defineConfig({
-  plugins: [svelte(), imagetools(), glsl()],
+export default defineConfig(({ mode }) => ({
+  plugins: [svelte(), imagetools(), glsl({ compress: mode === 'production' })],
   css: {
     preprocessorOptions: { scss: { api: 'modern' } }
   },
-})
+}))
