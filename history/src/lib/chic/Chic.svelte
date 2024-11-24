d2cf1b7a18b9510e3004fd14ec6a36d47e6295cd
Éric NICOLAS <ccjmne@gmail.com>
2024-11-24 20:34:01 +0100

    Dump debug info on shader compilation error

--- history/src/lib/chic/Chic.svelte
+++ history/src/lib/chic/Chic.svelte
@@ -13,7 +13,7 @@
       return
     }
 
-    const program = createProgram(
+    const program = createProgramme(
       gl,
       createShader(gl, gl.VERTEX_SHADER, vert),
       createShader(gl, gl.FRAGMENT_SHADER, frag)
@@ -52,19 +52,25 @@
     const shader = gl.createShader(type)!
     gl.shaderSource(shader, source)
     gl.compileShader(shader)
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      throw new Error(`Vertex shader failed to compile: ${gl.getShaderInfoLog(shader)}`)
+    }
     return shader
   }
 
-  function createProgram(
+  function createProgramme(
     gl: WebGLRenderingContext,
-    vertexShader: WebGLShader,
-    fragmentShader: WebGLShader
+    vertex: WebGLShader,
+    fragment: WebGLShader
   ): WebGLProgram {
-    const program = gl.createProgram()!
-    gl.attachShader(program, vertexShader!)
-    gl.attachShader(program, fragmentShader!)
-    gl.linkProgram(program)
-    return program
+    const programme = gl.createProgram()!
+    gl.attachShader(programme, vertex)
+    gl.attachShader(programme, fragment)
+    gl.linkProgram(programme)
+    if (!gl.getProgramParameter(programme, gl.LINK_STATUS)) {
+      throw new Error(`Shader program failed to link: ${gl.getShaderInfoLog(programme)}`)
+    }
+    return programme
   }
 </script>
 
